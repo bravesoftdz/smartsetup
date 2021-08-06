29dd1822fd152550b9c42bcc8a8875dbc640684b
faag_@live.com <ryuuzera>
2021-08-06 19:39:13 -0300

    first progress

--- SmartSetup.dpr
+++ SmartSetup.dpr
@@ -2,13 +2,16 @@ program SmartSetup;
 
 uses
   Vcl.Forms,
-  SmartSetup.View.Main in 'src\View\SmartSetup.View.Main.pas' {Form1};
+  SmartSetup.View.Main in 'src\View\SmartSetup.View.Main.pas' {frmInicio},
+  SmartSetup.View.Download in 'src\View\SmartSetup.View.Download.pas' {frmDownload},
+  SmartSetup.Model.InterfaceCallback in 'src\Model\SmartSetup.Model.InterfaceCallback.pas';
 
 {$R *.res}
 
 begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
-  Application.CreateForm(TForm1, Form1);
+  Application.CreateForm(TfrmInicio, frmInicio);
+  Application.CreateForm(TfrmDownload, frmDownload);
   Application.Run;
 end.
